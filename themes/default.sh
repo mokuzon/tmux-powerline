1d3b39628b38f14779ee8c5a48690533c8c9bd59
Motoi Okuzono <motoi-okuzono@cookpad.com>
2017-07-05 22:59:18 +0900

    Update theme

--- themes/default.sh
+++ themes/default.sh
@@ -24,13 +24,13 @@ TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SEPARATOR=${TMUX_POWERLINE_DEFAULT_RIGHTSIDE_SE
 if [ -z $TMUX_POWERLINE_LEFT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_LEFT_STATUS_SEGMENTS=(
 		"tmux_session_info 148 234" \
-		"hostname 33 0" \
+		#"hostname 33 0" \
 		#"ifstat 30 255" \
 		#"ifstat_sys 30 255" \
 		"lan_ip 24 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		"wan_ip 24 255" \
-		"vcs_branch 29 88" \
-		"vcs_compare 60 255" \
+		"vcs_branch 106 255" \
+		"vcs_compare 1 255" \
 		"vcs_staged 64 255" \
 		"vcs_modified 9 255" \
 		"vcs_others 245 0" \
@@ -40,20 +40,20 @@ fi
 if [ -z $TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS ]; then
 	TMUX_POWERLINE_RIGHT_STATUS_SEGMENTS=(
 		#"earthquake 3 0" \
-		"pwd 89 211" \
+		#"pwd 89 211" \
 		#"macos_notification_count 29 255" \
-		"mailcount 9 255" \
-		"now_playing 234 37" \
+		#"mailcount 9 255" \
+		"now_playing 182 199" \
 		#"cpu 240 136" \
-		"load 237 167" \
-		#"tmux_mem_cpu_load 234 136" \
-		"battery 137 127" \
+		#"load 237 167" \
+		"tmux_mem_cpu_load 186 202" \
+		"battery 178 127" \
 		"weather 37 255" \
 		#"rainbarf 0 ${TMUX_POWERLINE_DEFAULT_FOREGROUND_COLOR}" \
 		#"xkb_layout 125 117" \
-		"date_day 235 136" \
-		"date 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
-		"time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
+		"date 238 255" \
+		"date_day 238 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
+		"time 238 255 ${TMUX_POWERLINE_SEPARATOR_RIGHT_THIN}" \
 		#"utc_time 235 136 ${TMUX_POWERLINE_SEPARATOR_LEFT_THIN}" \
 	)
 fi
